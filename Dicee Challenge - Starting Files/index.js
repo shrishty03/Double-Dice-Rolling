d1da880cfbdab6ae45eb46cb1398568ef8f1c2eb
Shrishty Maurya <83492801+shrishty03@users.noreply.github.com>
2023-08-03 23:46:16 +0530

    Dice rolling-1

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6 random numbers
+
+var randomDiceImage = "dice"+ randomNumber1 + ".png"; //dice1-6.png
+
+var randomImageSource1 = "images/" + randomDiceImage; //images/dice1-6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if (randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player1 Wins";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player2 Wins";
+}
+else
+{
+    document.querySelector("h1").innerHTML="DRAW";
+}
